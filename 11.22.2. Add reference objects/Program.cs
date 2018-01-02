63b3b8cc142dae84cb5799dafa255ea5b469d389
erolcalisgan <erolcalisgan@hotmail.com>
2018-01-02 17:35:45 +0300

    Data Structure

---
+++ 11.22.2. Add reference objects/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+
+//11.22.2.	Add reference objects to ArrayList
+
+
+class MyClass
+{
+    public string MyName = "";
+}
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        ArrayList classList = new ArrayList();
+        classList.AddRange(new MyClass[] { new MyClass(),
+                                           new MyClass(),
+                                           new MyClass()});
+        Console.WriteLine("Items in List: {0}", classList.Count);
+
+        // Print out current values. 
+        foreach (MyClass c in classList)
+        {
+            Console.WriteLine("MyClass name: {0}", c.MyName);
+        }
+    }
+}
+//Items in List: 3
+//MyClass name:
+//MyClass name:
+//MyClass name:
---
+++ 11.22.2. Add reference objects/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("11.22.2. Add reference objects")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("11.22.2. Add reference objects")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("adde9f90-0156-4cb8-95f1-12a03439d970")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
