b741a8f0a2ddedeb6876f7ef1c7987d17e32f3cd
erolcalisgan <erolcalisgan@hotmail.com>
2018-01-18 16:53:51 +0300

    Data Struct

---
+++ 11.32.1. Use the keys/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+//	11.32.1.Use the keys to obtain the values from a generic SortedDictionary class.
+
+
+class MainClass
+{
+    public static void Main()
+    {
+        SortedDictionary<string, double> dict = new SortedDictionary<string, double>();
+
+        dict.Add("A", 7);
+        dict.Add("B", 5);
+        dict.Add("C", 4);
+        dict.Add("D", 9);
+
+        // Get a collection of the keys (names). 
+        ICollection<string> c = dict.Keys;
+        ICollection<double> d = dict.Values;
+
+        foreach (string str in c)
+            Console.WriteLine("{0}, Salary: {1:C}", str, dict[str]);
+    }
+}
+//A, Salary: $7.00
+//B, Salary: $5.00
+//C, Salary: $4.00
+//D, Salary: $9.00
---
+++ 11.33.1. Create a SortedList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+
+
+//11.33.1.	Create a SortedList using the default comparer
+
+
+public class SamplesSortedList
+{
+    public static void Main()
+    {
+
+        SortedList mySL1 = new SortedList(3);
+        Console.WriteLine("mySL1 (default):");
+        mySL1.Add("FIRST", "Hello");
+        mySL1.Add("SECOND", "World");
+        mySL1.Add("THIRD", "!");
+        try
+        {
+            mySL1.Add("first", "Ola!");
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e);
+        }
+        PrintKeysAndValues(mySL1);
+
+    }
+
+    public static void PrintKeysAndValues(SortedList myList)
+    {
+        Console.WriteLine("        -KEY-   -VALUE-");
+        for (int i = 0; i < myList.Count; i++)
+        {
+            Console.WriteLine("{0,-6}: {1}", myList.GetKey(i), myList.GetByIndex(i));
+        }
+    }
+}
