3716f488a0c88b306fdf90cdadfe8b7f748ec9ab
erolcalisgan <erolcalisgan@hotmail.com>
2018-01-23 16:57:23 +0300

    Data Structure

---
+++ GetCreationTime and GetLastAccessTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+class MainClass
+{
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+     static void Main()
+    {
+        OpenFileDialog dlgOpen = new OpenFileDialog();
+        if (dlgOpen.ShowDialog() == DialogResult.OK)
+        {
+            string s = dlgOpen.FileName;
+            Console.WriteLine("Filename " + s);
+            Console.WriteLine(" Created at " + File.GetCreationTime(s));
+            Console.WriteLine(" Accessed at " + File.GetLastAccessTime(s));
+        }
+
+    }
+
+}
+
+//Filename C:\TEST\test.txt
+// Created at 27.11.2017 13:31:35
+// Accessed at 27.11.2017 13:31:35
